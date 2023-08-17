d9dbaa9c0ee90740cc2f0e18642a1b386ce83dfe
rafipopz98 <itsrealrafi@gmail.com>
2023-08-17 18:36:50 +0530

    scrollbar

---
+++ app/components/Categories/Categories.jsx
@@ -0,0 +1,77 @@
+"use client";
+import "./Categories.css";
+import image0 from '../../../public/image0.jpeg'
+import image1 from '../../../public/image1.jpeg'
+import image2 from '../../../public/image2.jpeg'
+import image3 from '../../../public/image3.jpeg'
+import image4 from '../../../public/image4.jpeg'
+import image5 from '../../../public/image5.jpeg'
+import Link from "next/link";
+import Image from "next/image";
+const Categories = () => {
+  return (
+    <div className="categories">
+      <div className="col">
+        <div className="row">
+            <Image src={image0} alt="" />
+            <button>
+                <Link className="link" href='/products/men'>
+                    Mens
+                </Link>
+            </button>
+        </div>
+        <div className="row">
+        <Image src={image1} alt="" />
+            <button>
+                <Link className="link" href='/products/men'>
+                    Casual
+                </Link>
+            </button>
+        </div>
+      </div>
+      <div className="col"> 
+        <div className="row">
+        <Image src={image2} alt="" />
+            <button>
+                <Link className="link" href='/products/men'>
+                    Cargo
+                </Link>
+            </button>
+        </div>
+      </div>
+      <div className="col col-big"> 
+        <div className="row">
+          <div className="col">
+            <div className="row">
+            <Image src={image3} alt="" />
+            <button>
+                <Link className="link" href='/products/men'>
+                    Tees
+                </Link>
+            </button>
+            </div> 
+          </div>
+          <div className="col">
+            <div className="row">
+            <Image src={image4} alt="" />
+            <button>
+                <Link className="link" href='/products/men'>
+                    Accessories
+                </Link>
+            </button>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+        <Image src={image5} alt="" />
+            <button>
+                <Link className="link" href='/products/men'>
+                    Shoes
+                </Link>
+            </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Categories;
--- app/page.js
+++ app/page.js
@@ -2,12 +2,14 @@
 
 import Featured from "./components/FeaturedProducts/Featured"
 import Slide1 from "./components/Slider/Slide1"
+import Categories from "./components/Categories/Categories"
 
 export default function Home() {
   return (
    <div className="home">
     <Slide1 />
     <Featured heading="Featured"/>
+    <Categories />
     <Featured heading="Sale"/>
    </div> 
   )
