c43f1748c37dad6145f945ff90efa08bbea97472
rafipopz98 <itsrealrafi@gmail.com>
2023-09-01 23:42:27 +0530

    add prducts backend

--- src/app/admin-view/addproduct/page.js
+++ src/app/admin-view/addproduct/page.js
@@ -6,41 +6,107 @@ import {
   firebaseStorage,
 } from "@/src/components/FeaturedProducts/Projects";
 import { initializeApp } from "firebase/app";
-import { getStorage } from "firebase/storage";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+
 const app = initializeApp(firebaseConfig);
+
 const storage = getStorage(app, firebaseStorage);
+
 const AdminViewAddProduct = () => {
-  const handleImage = async (event) => {
-    console.log(event.target.files);
+  const uniqueFileName = async (getfile) => {
+    const timeStamp = Date.now();
+    const randomStringValue = Math.random().toString(36).substring(2, 12);
+
+    return `${getfile.name}-${timeStamp}-${randomStringValue}`;
   };
+
+  const helperforUploadingImageForFirebase = async (file) => {
+    const getFileName = uniqueFileName(file);
+    const storageRef = ref(storage, `ecommerce/${getFileName}`);
+
+    const uploadImage = uploadBytesResumable(storageRef, file);
+
+    return new Promise((resolve, reject) => {
+      uploadImage.on(
+        `state_changed`,
+        (snapshot) => {},
+        (error) => {
+          console.log(`the errror is ${error}`);
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadImage.snapshot.ref)
+            .then((downloadUrl) => resolve(downloadUrl))
+            .catch((error) => reject(error));
+        }
+      );
+    });
+  };
+
   let name, value;
+
+  
+  const [selected, setSelected] = useState('');
+
+  
+  const [formData, setFormData] = useState({
+    image: "",
+    name: "",
+    price: 0,
+    desc: "",
+    category: '',
+    DInfo: "",
+
+    pDrop: 0,
+  });
 
   const inputHandle = (e) => {
     console.log("gg");
     name = e.target.name;
     value = e.target.value;
+    // console.log("some stuff", formData.category);
     setFormData({
       ...formData,
       [name]: value,
     });
   };
-  const [formData, setFormData] = useState({
-    image: "",
-    name: "",
-    price: "",
-    desc: "",
-    category: {
-      tees: "",
-      cargo: "",
-      hoodies: "",
-      pants: "",
-      accessories: "",
-    },
-    DInfo: "",
 
-    pDrop: "",
-  });
+  const handleChange = event => {
+    console.log('Label 👉️', event.target.selectedOptions[0].label);
+    const noice= setSelected(event.target.value)
+    setFormData({
+      ...formData,
+      category:noice
+    });
+    // console.log(formData)
+    // console.log(event.target.value);
+  };
 
+  const handleImage = async (event) => {
+    console.log(event.target.files);
+    const extractimageUrl = await helperforUploadingImageForFirebase(
+      event.target.files[0]
+    );
+    console.log(extractimageUrl);
+
+    if (extractimageUrl !== "") {
+      setFormData({
+        ...formData,
+        image: extractimageUrl,
+      });
+    }
+  };
+
+  console.log("selecteddd",selected);
+  console.log("all the data",formData);
+
+  
+console.log()
   return (
     <div className="container_addProducts">
       <input
@@ -70,6 +136,7 @@ const AdminViewAddProduct = () => {
         value={formData.price}
         name="price"
         placeholder="Enter Product Price"
+        onChange={inputHandle}
       />
       <input
         type="text"
@@ -77,13 +144,25 @@ const AdminViewAddProduct = () => {
         value={formData.desc}
         name="desc"
         placeholder="Enter About Product"
+        onChange={inputHandle}
       />
-      <select placeholder="Category">
-        <option value={formData.category.tees}>Tees</option>
-        <option value={formData.category.cargo}>Cargo</option>
-        <option value={formData.category.pants}>Pants</option>
-        <option value={formData.category.hoodies}>Hoodies</option>
-        <option value={formData.category.accessories}>lol</option>
+      <select
+        placeholder="Category"
+        value={formData.category}
+        onChange={handleChange}
+      >
+      <option value="">--Choose an option--</option>
+        <option name="tees" id="tees" value="tees">Tees</option>
+        <option id="cargo" value="cargo">
+          Cargo
+        </option>
+        <option  id="pants" value="pants">
+          Pants
+        </option>
+        <option id="hoodies" value="hoodies">
+          Hoodies
+        </option>
+        {/* <option value={formData.category}>lol</option> */}
       </select>
       <input
         type="text"
@@ -91,13 +170,15 @@ const AdminViewAddProduct = () => {
         value={formData.DInfo}
         name="DInfo"
         placeholder="Enter Delivery Info"
+        onChange={inputHandle}
       />
       <input
-        type="text"
+        type="number"
         id="pDrop"
         value={formData.pDrop}
         name="pDrop"
         placeholder="Enter Price Drop"
+        onChange={inputHandle}
       />
       <button>ADD PRODUCT</button>
     </div>
