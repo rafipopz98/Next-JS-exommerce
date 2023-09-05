21a8a006629e3d22a8b0c6c44670979284216630
rafipopz98 <itsrealrafi@gmail.com>
2023-09-05 23:46:44 +0530

    delet and update api and service

---
+++ src/app/api/admin/updateProduct/route.js
@@ -0,0 +1,51 @@
+import NextResponse from "next/server";
+import connectionDb from "@src/database";
+import Products from "@/src/models/product";
+
+export const dynamic = "force-dynamic";
+
+export async function PUT() {
+  try {
+    await connectionDb();
+    const extractData = await req.json();
+
+    const { _id, image, name, price, desc, category, DInfo, pDrop, sizes } =
+      extractData;
+    const updateProduct = await Products.findOneAndUpdate(
+      {
+        _id: _id,
+      },
+      {
+        image,
+        name,
+        price,
+        desc,
+        category,
+        DInfo,
+        pDrop,
+        sizes,
+      },
+      { new: true }
+    );
+
+    if (updateProduct) {
+      return NextResponse.json({
+        success: true,
+        message: "Product updates successfully",
+      });
+    } else {
+      console.log("second error");
+      return NextResponse.json({
+        success: false,
+        message: "Failed to update the product ! please try again",
+      });
+    }
+  } catch (error) {
+    console.log("fourth");
+    console.log(error);
+    return NextResponse.json({
+      success: false,
+      message: "Something went wrong ! Please try again later",
+    });
+  }
+}
--- src/services/product/index.js
+++ src/services/product/index.js
@@ -27,12 +27,48 @@ export const getAllAdminroducts = async () => {
       "http://localhost:3000/api/admin/allProducts",
       {
         method: "GET",
-        cache:"no-store"
+        cache: "no-store",
       }
     );
     const data = await responses.json();
     return data;
   } catch (e) {
     console.log(`the errrror ${e}`);
+  }
+};
+
+//update the product
+
+export const updateProduct = async (formData) => {
+  try {
+    const responses = await fetch("/api/admin/updateProduct", {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${Cookies.get("token")}`,
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await responses.json();
+    return data;
+  } catch (e) {
+    console.log(`the errrror ${e}`);
+  }
+};
+
+//delete product api
+
+export const deleteProduct = async (id) => {
+  try {
+    const pDelete = await fetch(`/api/admin/deleteProduct?id=${id}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${Cookies.get("token")}`,
+      },
+    });
+    const data = await responses.json();
+    return data;
+  } catch (e) {
+    console.log("error occured while deletin product ", e);
   }
 };
