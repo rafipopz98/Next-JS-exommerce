1b3e07237ff3c69dda60e1fbb5eaa1753366bf54
rafipopz98 <itsrealrafi@gmail.com>
2023-09-22 20:42:19 +0530

    netlify

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -3,15 +3,19 @@ import React from "react";
 import "./List.css";
 import Card from "../Card/Card";
 import ProductButtons from "../SingleProduct/ProductButtons/page";
+import Notification from "../Notification";
 const List = ({ data }) => {
   return (
     <div className="lists">
-      {data.map((item,id) => (
-        <article key={id} className="ariticleList">
-          <Card item={item} />
-          <ProductButtons item={item} />
-        </article>
-      ))}
+      {data && data.length
+        ? data.map((item) => (
+            <article  key={item._id} className="ariticleList">
+              <Card item={item} />
+              <ProductButtons item={item} />
+            </article>
+          ))
+        : null}
+        <Notification/>
     </div>
   );
 };
