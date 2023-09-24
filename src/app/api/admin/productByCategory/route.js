4c095389d016729d611a27e7fa406770dc40b5b4
rafipopz98 <itsrealrafi@gmail.com>
2023-09-24 23:50:03 +0530

    backend fetch

--- src/app/api/admin/productByCategory/route.js
+++ src/app/api/admin/productByCategory/route.js
@@ -2,40 +2,36 @@ import { NextResponse } from "next/server";
 import { connectionDb } from "../../../../../src/database";
 import Products from "../../../../../src/models/product";
 
-
-
 export const dynamic = "force-dynamic";
 
-
-export async function GET(req){
+export async function GET(req) {
+  try {
     await connectionDb();
 
     const { searchParams } = new URL(req.url);
     const id = searchParams.get("id");
 
-    const getData=await Products.find({category:id})
+    const getData = await Products.find({ category: id });
 
-    if (deleteProduct) {
-        return NextResponse.json({
-          success: true,
-          data:getData,
-          message: "successfully found",
-        });
-      } else {
-        console.log("f");
-        return NextResponse.json({
-          success: false,
-          status:204,
-          message: "NO  product found",
-        });
-      }
-}
-try{
-
-}catch(error){
-    console.log("fourth cheeee",error);
+    if (getData) {
+      return NextResponse.json({
+        success: true,
+        data: getData,
+        message: "successfully found",
+      });
+    } else {
+      console.log("f");
+      return NextResponse.json({
+        success: false,
+        status: 204,
+        message: "NO  product found",
+      });
+    }
+  } catch (error) {
+    console.log("fourth cheeee", error);
     return NextResponse.json({
       success: false,
       message: "Something went wrong ! Please try again later",
     });
+  }
 }
